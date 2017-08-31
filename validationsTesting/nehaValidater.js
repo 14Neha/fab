39b407c1d1c27e919b883b6b4da8372cec4b22bf
14neha <14neharathore@gmail.com>
2017-08-31 10:27:45 +0530

    ..

--- validationsTesting/nehaValidater.js
+++ validationsTesting/nehaValidater.js
@@ -4,7 +4,12 @@ function mainValidate() {
   {
     if(eles[i].getAttribute("domain").length)
     {
-      if(/^(([0-9]{0,7})|[a-z.0-9]{1,30})[@][a-z0-9]{2,63}([.][a-z]{2,10})+$/g.test(eles[i].value))
+      email12();
+    }
+  }
+}
+function email12(){
+	if(/^(([0-9]{0,7})|[a-z.0-9]{1,30})[@][a-z0-9]{2,63}([.][a-z]{2,10})+$/g.test(eles[i].value))
       {
           eles[i].title="perfect";
           eles[i].style.backgroundColor="rgba(0,255,0,0.2)";
@@ -14,6 +19,4 @@ function mainValidate() {
             eles[i].title="Error";
             eles[i].style.backgroundColor="rgba(255,0,0,0.2)"
       }
-    }
-  }
 }
