828b5c2394c2f43050ac3ee5ebba20fdc8ee55c5
mayankmandloi <mayankmandloi009@gmail.com>
2017-09-21 11:02:09 +0530

    orderBy-2

---
+++ Angular/Day-6/day-6.js
@@ -0,0 +1,36 @@
+var myApp=angular.module("WebBhumi",[]);
+var classes=function ($scope) {
+  var students = [{name:"Raj",course:"Angular",batch:"08:00 pm",like:0,dislike:0 },
+  {name:"Brijesh",course:"Web Design",batch:"12:00 pm",like:23,dislike:0}, {name:"Darshay",course:"Web Design",batch:"11:00 am",like:0,dislike:0}, {name:"Shalini",course:"Web Design",batch:"04:00 pm",like:0,dislike:0},
+  {name:"Aruna",course:"All",batch:"09:00 am",like:0,dislike:0 },
+  {name:"Jayesh",course:"All",batch:"12:00 am",like:0,dislike:0 },
+  {name:"AKhlesh",course:"Angular",batch:"08:00 pm",like:0,dislike:0 }];
+  $scope.students=students;
+  $scope.increseLike=function (that) {
+    that.like++;
+  };
+  $scope.increseDisLike=function (that) {
+    that.dislike++;
+  }
+$scope.setSelectedValue=function (value) {
+  $scope.selectedValue=value;
+}
+};
+myApp.controller("classes",classes);
+
+
+var coursesFunc= function($scope) {
+  var courses = [{name:"Angular",covers:[{name:"JavaScript"},{name:"Angular"}]},{name:"Web Design",covers:[{name:"html"},{name:"css"},{name:"JavaScript"},{name:"Angular"}]}];
+  $scope.courses=courses;
+  $scope.filterByMe=function(item) {
+    if(item.name==$scope.st.name||$scope.st.name=="All")
+    {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+myApp.controller("courses",coursesFunc);
